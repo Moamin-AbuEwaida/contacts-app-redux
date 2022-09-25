f16c5ecd56fe1271dd9b2d00569ffab2a929b8b7
Moamin <moamin.salamah@gmail.com>
2022-09-25 10:32:21 +0200

    creating the routes links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
 import './App.css';
 import { ToastContainer} from 'react-toastify';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <h1>Hello world</h1>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<h1>Welcome to my contacts list</h1>} />
+        <Route exact path='/add' element={<h1>Add</h1>} />
+        <Route exact path='/edit/:id' element={<h1>Edit</h1>} />
+          
+      </Routes>
     </div>
   );
 }

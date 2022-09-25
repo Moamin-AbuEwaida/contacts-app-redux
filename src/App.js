644cf15236f3e16c0b8cd361c73458bc27313641
Moamin <moamin.salamah@gmail.com>
2022-09-25 10:05:12 +0200

    setting up the project folders and dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { ToastContainer} from 'react-toastify';
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;

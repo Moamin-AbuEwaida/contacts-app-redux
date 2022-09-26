5f4720e2520e4d6632214200af841fea36a921ae
Moamin <moamin.salamah@gmail.com>
2022-09-26 09:36:10 +0200

    finishing home page table display contacts calling from redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const Home = () => {
+    const contacts = useSelector(state=>state);
+
   return (
     <div className="container">
         <div className="row">
@@ -10,8 +13,32 @@ const Home = () => {
                     Add Contact
                 </Link>
             </div>
-            <div className="col-md-6 mx-auto">
-                <h1>Welcome to the contacts list</h1>
+            <div className="col-md-12 mx-auto">
+                <table className="table table-hover">
+                    <thead className="text-white bg-dark text-center">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Number</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contacts.map((contact,id)=>(
+                            <tr key={id}>
+                                <td>{id + 1}</td>
+                                <td>{contact.name}</td>
+                                <td>{contact.email}</td>
+                                <td>{contact.number}</td>
+                                <td>
+                                    <Link to={`/edit/${contact.id}`} className="btn btn-small btn-primary">Edit</Link>
+                                    <button type='button' className="btn btn-small btn-danger mx-2">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
